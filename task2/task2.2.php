92e567dda2f14f9ba9c3511ddfe7bb6207acd83e
Infernarium <mark.tumanyan@yandex.ru>
2023-02-18 14:37:52 +0300

    add task2.2

---
+++ task2/task2.2.php
@@ -0,0 +1,25 @@
+<?php
+
+// тип содержимого - jpg
+header('Content-Type: image/jpeg');
+
+// открытие файла с картинкой и связываем его с идентификатором $im
+$im = imagecreatefromjpeg("./samples/1.jpg");
+
+// создание цвета текста
+$text_color = imagecolorallocate($im, 255, 65, 91);
+
+// прорисовка строки текста шрифтом (латиница!) 5 в координаты 5, 5
+imagestring($im, 5, 250, 350, "HELLO WORLD", $text_color);
+
+$text = 'Вставить текст';
+$font = 'arial.ttf';
+$color = imagecolorallocate($im, 255, 255, 255);
+imagettftext($im, 36, 0, 65, 380, $black, $font, $text);
+
+// вывод изображения в браузер
+imagejpeg($im, "demotivator.jpg");
+
+// освобождение памяти
+imagedestroy($im);
+?>
