f51c4872eedd819c15a58cae79370bcf04b35854
Infernarium <mark.tumanyan@yandex.ru>
2023-02-20 00:50:50 +0300

    fix task 2.2

--- task2/task2.2.php
+++ task2/task2.2.php
@@ -1,23 +1,16 @@
 <?php
-
-// тип содержимого - jpg
 header('Content-Type: image/jpeg');
 
-// открытие файла с картинкой и связываем его с идентификатором $im
-$im = imagecreatefromjpeg("./1.jpg");
+$im = imagecreatefromjpeg("1.jpg");
 
-// создание цвета текста
 $text_color = imagecolorallocate($im, 255, 255, 255);
 
-// Текст надписи
 $text = 'ЕГЭ ПО РИСОВАНИЮ';
-// Замена пути к шрифту на пользовательский
 $font = './arial.ttf';
 $file = 'demotivator.jpg';
-// прорисовка строки текста шрифтом (латиница!) 5 в координаты 5, 5
 
-imagettftext($im, 36, 0, 65, 380, $text_color, realpath($font),$text);
-// вывод изображения в браузер
-imagejpeg($im,'demotivator.jpg');
+imagettftext($im, 36, 0, 65, 380, $text_color, realpath($font), $text);
+
+imagejpeg($im, 'demotivator.jpg');
 imagedestroy($im);
 ?>
